572f5465d8ee7b67a8860c114cf262e11e576d40
Reynaldo-Falcon <falcon.reynaldo20@gmail.com>
2025-04-27 17:59:08 -0400

    Added Title and Author Text while also slightly moving buttons down

--- FDForPDEsSimulationCode.js
+++ FDForPDEsSimulationCode.js
@@ -8,6 +8,11 @@
     const canvasPosX = 0;
     const canvasPosY = 0;
 
+// Title and Author Text Related
+
+    const textAnchorX = canvasWidth/2;  // Provides anchor for all text horizontally: text spawn from the center
+    const textAnchorY = canvasHeight/8; // Provides anchor for all text vertically: text spawns from the top
+
 // Menu buttons related
 
     const menuButWidth = 150;
@@ -23,7 +28,7 @@
     const wButPosX = canvasPosX + menuButMiddleAnchorX + canvasWidth*(2/4);    // Second Button
 
     // They all have the same y coordinate!
-    const menuButPosY = canvasPosY + menuButMiddleAnchorY + canvasHeight/2;
+    const menuButPosY = canvasPosY + menuButMiddleAnchorY + canvasHeight*(3/5);
 
 // ------Program------
 
@@ -33,6 +38,19 @@ function setup()
     canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.position(canvasPosX, canvasPosY);
     canvas.background(0, 120, 80);  // Forest Teal color
+
+    // Title Text
+    textAlign(CENTER, CENTER);  // Will place text's anchor in center for both title AND author text
+    textSize(48);
+    fill(255);
+    text("Finite Difference Method", textAnchorX, textAnchorY);
+    text("for", textAnchorX, textAnchorY + 45);
+    text("PDE Simulations", textAnchorX, textAnchorY + 90);
+
+    // Author Text
+    textSize(24);
+    fill(200);
+    text("Reynaldo J. Falcón Torres & Edgardo A. Diaz Sandoval ", textAnchorX, textAnchorY + 140);
 
     // Create simulation buttons
     const laplaceButton = createButton('Laplace Simulation');
